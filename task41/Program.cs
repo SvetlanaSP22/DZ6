e2d011ec13fe231ed10cded0ccfea1f44f0711d3
Svetlana <sypkovas@mail.ru>
2023-03-05 12:30:48 +0300

    добавили task41

---
+++ task41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+
+int CountNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            result++;
+    }
+    return result;
+}
+
+
+Console.WriteLine("Введите несколько целых чисел, отделяя их пробелами: ");
+int result = CountNumbers(number);
+Console.Write($" -> {CountNumbers}");
+
